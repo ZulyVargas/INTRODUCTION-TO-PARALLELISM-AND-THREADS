a219ff584bc7a8e8927891baffb7c1529d26c85d
unknown <zuly.vargas@labinfo.is.escuelaing.edu.co>
2022-01-19 09:33:30 -0500

    Ejercicio Parte 1

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -3,12 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package edu.eci.arsw.threads;
+
+import java.lang.Thread;
 
 /**
  *
  * @author hcadavid
  */
-public class CountThread {
+public class CountThread extends Thread{
+	
+	private int numero1;
+	private int numero2;
     
+	CountThread (int A, int B){
+		numero1 = A;
+		numero2 = B;
+	}
+	
+	public void run() {
+		for (int i = numero1; i <= numero2; i++) {
+			System.out.print(i + " ");
+		}
+	}
+	
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -5,6 +5,7 @@
  */
 package edu.eci.arsw.threads;
 
+
 /**
  *
  * @author hcadavid
@@ -12,7 +13,20 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
+    	
+    	CountThread hilo1 = new CountThread(0 , 99);
+    	CountThread hilo2 = new CountThread(99 , 199);
+    	CountThread hilo3 = new CountThread(200 , 299);
         
+    	/*
+    	hilo1.start();
+    	hilo2.start();
+    	hilo3.start();
+    	*/
+    	hilo1.run();
+    	hilo2.run();
+    	hilo3.run();
+    	
     }
     
 }
