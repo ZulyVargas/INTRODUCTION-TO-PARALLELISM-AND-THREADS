7acb80e3a13de388c8ab0f897e00a66d9ac4ad0e
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-01-22 17:30:20 -0500

    Primer punto : Desarrollo primer punto

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -29,3 +29,5 @@ public class CountThread extends Thread{
 	}
 	
 }
+
+
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -17,12 +17,13 @@ public class CountThreadsMain {
     	CountThread hilo1 = new CountThread(0 , 99);
     	CountThread hilo2 = new CountThread(99 , 199);
     	CountThread hilo3 = new CountThread(200 , 299);
-        
-    	/*
+
+    	System.out.println("Start Threads (start()) : ");
     	hilo1.start();
     	hilo2.start();
     	hilo3.start();
-    	*/
+
+    	System.out.println("Start Threads (run()) : ");    	
     	hilo1.run();
     	hilo2.run();
     	hilo3.run();
